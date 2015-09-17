8e7736f6bfc959482c1ecad0820065192605a707
Aleksey Bielokonenko <vest.008@gmail.com>
2015-09-17 13:20:53 +0300

    map move through fix

--- public/js/controllers/map-transparent-controller.js
+++ public/js/controllers/map-transparent-controller.js
@@ -1,25 +1,59 @@
 angular.module('MTMonitor').controller('MapTransparentController', ['$scope', function (scope) {
-  var map;
+  var map,
+      position
+      inside = false,
+      windowEl = $(window),
+      holderEl = null;
 
   initMap();
-  setTransparent(4);
+  addListeners();
+  setTransparent();
 
-  function setTransparent(depth){
-    var el = $('#transparent-map-holder').parent();
-    for (var i = 0; i < depth; i++) {
-      console.log(el);
+  function checkMouseInRect(pos, x, y){
+    if(pos.top < y && pos.left < x &&
+        pos.top + pos.height > y && pos.left + pos.width > x){
+          // console.log('Inside!');
+          return true;
+        }
+    // console.log('Outside!');
+    return false;
+  }
+
+  function addListeners(){
+    $(window).resize(resize);
+    resize();
+
+    holderEl = $('#transparent-map-holder'),
+    holderEl.parent().on('mouseenter', function(event){
+      if(!inside){
+        position = holderEl.position();
+        position.width = holderEl.width();
+        position.height = holderEl.height();
+        inside = true;
+        $('.lm_goldenlayout').css('pointer-events', 'none');
+        // console.log(event);
+
+        $('body').mousemove(function(event) {
+          if(!checkMouseInRect(position, event.clientX, event.clientY)){
+            inside = false;
+            $('.lm_goldenlayout').css('pointer-events', 'auto');
+            $('body').off('mousemove');
+          }
+        });
+      }
+    });
+  }
+
+  function setTransparent(){
+    var el = holderEl.parent();
+
+    for (var i = 0; i < 4; i++) {
       el.css('opacity', '0');
       el = el.parent();
     }
   }
 
   function initMap(){
-    // var map = $('#map');
-    // if($('#map').length == 0) return;
-
-    $('#map').height($(window).height());
-    $('#map').width($(window).width());
-
     map = L.map('map').setView([50.4412776, 30.4671281], 11);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -27,6 +61,11 @@ angular.module('MTMonitor').controller('MapTransparentController', ['$scope', fu
         id: 't4ddy.229f0f41',
         accessToken: 'pk.eyJ1IjoidDRkZHkiLCJhIjoiZDJhZDRhM2E2NmMzZWNhZWM3YTlmMWZhOTYwNmJlMGUifQ.6GBanGLBka6DNFexeC3M6g'
     }).addTo(map);
+  }
+
+  function resize(){
+      $('#map').height($(window).height());
+      $('#map').width($(window).width());
   }
 
 }]);
