34aa722d0912f83ae84823aa2c2f6516dd4803ab
T4ddy <alexey.wh@gmail.com>
2015-09-21 14:20:55 +0300

    port argument

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var express = require('express'),
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-var router = require('./server/router');
+var router = require('./server/router'),
+	port = process.argv[2] || 9020;;
 
 // var soapManager = new soap('hd', 'QJQB8uxW');
 var soapManager = new soap('k00056.0', 'As123456');
 // var soapManager = new soap('meest.disp', 'dispmeest');
 soapManager.getDailyPlan();
 
-app.use('/', router).listen(process.env.PORT || 9020);
-console.log('Listening on port ' + (process.env.PORT || 9020) + '...');
+app.use('/', router).listen(process.env.PORT || port);
+console.log('Listening on port ' + (process.env.PORT || port) + '...');
