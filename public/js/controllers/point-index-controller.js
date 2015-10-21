be4e4f46babf9168010544beee0a286b79225a9e
T4ddy <alexey.wh@gmail.com>
2015-10-21 11:01:20 +0300

    .

--- public/js/controllers/point-index-controller.js
+++ public/js/controllers/point-index-controller.js
@@ -492,7 +492,7 @@ angular.module('MTMonitor').controller('PointIndexController', ['$scope', '$http
             var date = new Date();
             return date.getFullYear() +
                 ("0" + (date.getMonth() + 1)).slice(-2) +
-                ( ("0" + date.getDate())).slice(-2);
+                ( ("0" + (date.getDate() - 1)).slice(-2);
         }
 
         function updateProblemIndex(route) {
