6cd92441149809465ea9c41661601f77502fca1a
Alexey Bielokonenko <alexey.wh@gmail.com>
2015-11-24 13:51:38 +0200

    to host

--- public/js/controllers/point-index-controller.js
+++ public/js/controllers/point-index-controller.js
@@ -524,10 +524,13 @@ angular.module('MTMonitor').controller('PointIndexController', ['$scope', '$http
                 return;
             }
 
-            //console.log([_data.ID, getTodayStrFor1C()]);
+            console.log([_data.ID == '28' ? '28/2' : '28', getTodayStrFor1C()]);
             //TODO REMOVE
             _data.mobile_buttons = parentForm._call('getDriversActions', [_data.ID == '28' ? '28/2' : '28', getTodayStrFor1C()]);
             console.log('_data.mobile_buttons', _data.mobile_buttons);
+
+            console.log('28/2', parentForm._call('getDriversActions', ['28/2', getTodayStrFor1C()]));
+            console.log('28', parentForm._call('getDriversActions', ['28', getTodayStrFor1C()]));
 
             if (_data.mobile_buttons == undefined
                 || Object.keys(_data.mobile_buttons).length == 0) {
