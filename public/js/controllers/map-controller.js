66a3033a388b375101e652d2e6133880982cb834
kallko <kallko_andrey@yahoo.co.uk>
2016-07-10 01:19:15 +0300

    1.closed 1.16

--- public/js/controllers/map-controller.js
+++ public/js/controllers/map-controller.js
@@ -803,7 +803,7 @@ angular.module('MTMonitor').controller('MapController', ['$scope', '$rootScope',
             //   return "Hola"
             //};
 
-            $(window).onclose(function(){
+            $(window).beforeunload(function(){
                 http.post('./logout')
                     .success(function (data) {
                         console.log("complete");
