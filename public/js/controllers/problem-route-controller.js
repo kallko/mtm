0a6c0636d5de00b237410e2b2d11a52b1af41faa
Alexey Bielokonenko <alexey.wh@gmail.com>
2016-09-16 17:54:41 +0300

    1. Мелкий баг с отображением статистики

--- public/js/controllers/problem-route-controller.js
+++ public/js/controllers/problem-route-controller.js
@@ -15,12 +15,12 @@ angular.module('MTMonitor').controller('ProblemRouteController', ['$scope', '$ht
             http.get('./confirmonline/')
                 .success(function (data) {
                     if (data.status != "ok") alert("Отсутсвует связь с сервером ");
-                    console.log(rootScope.data.statistic,  "Статистика такая была", rootScope.data.server_time);
+                    //console.log(rootScope.data.statistic,  "Статистика такая была", rootScope.data.server_time);
                     rootScope.data.server_time = data.server_time;
                     rootScope.data.statistic = data.statistics;
                     rootScope.nowTime = rootScope.data.server_time;
-                    rootScope.$emit('holestatistic', rootScope.data.statistic);
-                    console.log(rootScope.data.server_time, "Статистика такая стала", rootScope.data.statistic);
+                    //rootScope.$emit('holestatistic', rootScope.data.statistic);
+                    //console.log(rootScope.data.server_time, "Статистика такая стала", rootScope.data.statistic);
                 }).error(function () {
                     //rootScope.errorNotification('Нет связи с сервером');
                 });
