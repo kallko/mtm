9fb58d42752a8607052b430fc49c4502a08a1d8d
Alexey Bielokonenko <alexey.wh@gmail.com>
2016-12-26 22:42:10 +0200

    1.Отображение заметок от водителя 2 Загтовка к сигналу водителя

--- server/routers/mtm-router.js
+++ server/routers/mtm-router.js
@@ -7234,7 +7234,7 @@ function waitingCallForOperator(key, company){
         }
     }
 
-    if (result == null && cashedDataArr[company].waitingCalls && cashedDataArr[company].waitingCalls.length > 0){
+    if (result == {} && cashedDataArr[company].waitingCalls && cashedDataArr[company].waitingCalls.length > 0){
         result = cashedDataArr[company].waitingCalls[0];
         cashedDataArr[company].waitingCalls.splice(0,1);
     }
