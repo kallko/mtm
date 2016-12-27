69d6917420fed3ffc57298dc9841ec9a2ef6f7a4
Alexey Bielokonenko <alexey.wh@gmail.com>
2016-12-27 11:03:01 +0200

    Comment 7237

--- server/routers/mtm-router.js
+++ server/routers/mtm-router.js
@@ -7234,7 +7234,7 @@ function waitingCallForOperator(key, company){
         }
     }
 
-    console.log("Checking Waiting calls".blue,  cashedDataArr[company].waitingCalls != undefined , cashedDataArr[company].waitingCalls.length > 0 );
+    //console.log("Checking Waiting calls".blue,  cashedDataArr[company].waitingCalls != undefined , cashedDataArr[company].waitingCalls.length > 0 );
     if (result == undefined && cashedDataArr[company].waitingCalls != undefined && cashedDataArr[company].waitingCalls.length > 0){
         result = cashedDataArr[company].waitingCalls[0];
         cashedDataArr[company].waitingCalls.splice(0,1);
