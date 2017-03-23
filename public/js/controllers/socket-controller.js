4c62cdcedf5fd26fd46bd37e388d831faf79711a
kalko <kallko_andrey@yahoo.co.uk>
2017-03-23 19:54:55 +0200

    Merge branch 'feature/dispatchers' into develop
    Conflicts:
    	server/routers/mtm-router-new.js

--- public/js/controllers/socket-controller.js
+++ public/js/controllers/socket-controller.js
@@ -3,8 +3,8 @@
  */
 angular.module('MTMonitor').controller('SocketController', ['$scope', '$rootScope', '$filter', function (scope, rootScope, http) {
     console.log("SOCKET.IO");
-    //var socket = io('https://sngtrans.com.ua/monitoring');
-    var socket = io('https://google.com.ua/monitoring/');
+    var socket = io('https://sngtrans.com.ua/',['/monitoring/']);
+    //var socket = io('https://google.com.ua/monitoring/');
     //var socket = io();
     socket.on('dispatchers', function (data) {
            console.log(data);
