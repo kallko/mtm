8af68c9d01832dc9462ba2af72a56d3492464664
kalko <kallko_andrey@yahoo.co.uk>
2017-03-24 14:55:51 +0200

    Возврат на http

--- public/js/controllers/socket-controller.js
+++ public/js/controllers/socket-controller.js
@@ -1,33 +1,33 @@
 /**
  * Created by dev-2 on 14.02.17.
  */
-angular.module('MTMonitor').controller('SocketController', ['$scope', '$rootScope', '$filter', function (scope, rootScope, http) {
-    console.log("SOCKET.IO");
-    var socket = io('https://sngtrans.com.ua', {path: '/monitoring'});
-    //var socket = io('https://google.com.ua/monitoring/');
-    //var socket = io();
-    socket.on('dispatchers', function (data) {
-           console.log(data);
-        if (data.error) return;
-           rootScope.socketData = rootScope.socketData || {};
-           rootScope.socketData.dispatchers = data;
-        //socket.emit('my other event', { my: 'data' });
-        scope.$emit('dispatchersLoad');
-    });
-
-//fixme to local experiment (upper code for true server)
-
 // angular.module('MTMonitor').controller('SocketController', ['$scope', '$rootScope', '$filter', function (scope, rootScope, http) {
 //     console.log("SOCKET.IO");
-//     var socket = io('http://localhost:9020');
+//     var socket = io('https://sngtrans.com.ua', {path: '/monitoring'});
+//     //var socket = io('https://google.com.ua/monitoring/');
+//     //var socket = io();
 //     socket.on('dispatchers', function (data) {
-//         console.log(data);
+//            console.log(data);
 //         if (data.error) return;
-//         rootScope.socketData = rootScope.socketData || {};
-//         rootScope.socketData.dispatchers = data;
+//            rootScope.socketData = rootScope.socketData || {};
+//            rootScope.socketData.dispatchers = data;
 //         //socket.emit('my other event', { my: 'data' });
 //         scope.$emit('dispatchersLoad');
 //     });
+
+//fixme to local experiment (upper code for true server)
+
+angular.module('MTMonitor').controller('SocketController', ['$scope', '$rootScope', '$filter', function (scope, rootScope, http) {
+    console.log("SOCKET.IO");
+    var socket = io('http://localhost:9020');
+    socket.on('dispatchers', function (data) {
+        console.log(data);
+        if (data.error) return;
+        rootScope.socketData = rootScope.socketData || {};
+        rootScope.socketData.dispatchers = data;
+        //socket.emit('my other event', { my: 'data' });
+        scope.$emit('dispatchersLoad');
+    });
 
 
     socket.on('sendReport', function (data) {
